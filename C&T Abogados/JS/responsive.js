9ca5cf206d73227c9316d7157fce07c06a830114
Mariano <136939689+MareanoSG@users.noreply.github.com>
2023-08-17 19:19:47 -0300

    Add files via upload

--- C&T Abogados/JS/responsive.js
+++ C&T Abogados/JS/responsive.js
@@ -8,7 +8,7 @@ function toggleMenu(element){
 }
 
 window.addEventListener('scroll', function() {
-    if (window.innerWidth <= 550) {
+    if (window.innerWidth <= 850) { // movil
         var element = document.getElementById('header_box');
         var scrollHeight = window.scrollY;
 
@@ -21,12 +21,12 @@ window.addEventListener('scroll', function() {
             document.getElementById('header_box').style.height = '20vh';
         }
     }
-    else if (window.innerWidth > 550){
+    else if (window.innerWidth > 850){ //pc
         var element = document.getElementById('header_box');
         var scrollHeight = window.scrollY;
 
         if (scrollHeight > 100) { 
-            this.document.getElementById('header_box').style.backgroundColor = '#f9f6f2f5';
+            this.document.getElementById('header_box').style.backgroundColor = '#f9f6f2f6';
             this.document.getElementById('header_box').style.display = 'flex';
             this.document.getElementById('header_box').style.flexDirection = 'row';
             this.document.getElementById('header_box').style.height = '12vh';
@@ -39,9 +39,9 @@ window.addEventListener('scroll', function() {
             this.document.getElementById('header_box').style.backgroundColor = '#F9F6F2';
             this.document.getElementById('header_box').style.display = 'flex';
             this.document.getElementById('header_box').style.flexDirection = 'column';
-            this.document.getElementById('header_box').style.height = '30vh';
+            this.document.getElementById('header_box').style.height = '25vh';
             this.document.getElementById('header_box').style.boxShadow = 'none';
-            this.document.querySelector('#header_box img').style.width = '180px';
+            this.document.querySelector('#header_box img').style.width = '150px';
             this.document.querySelector('#header_box img').style.marginRight = '0px';
             
         }
--- C&T Abogados/JS/translator.js
+++ C&T Abogados/JS/translator.js
@@ -15,13 +15,30 @@ function toggleIdiom(){
         document.getElementById('idiom_box-text').textContent = 'Ver página en Español 🇪🇸';
 
         document.querySelector('#main_box-1 h2').textContent = 'Understand. Support. Resolve.';
-        
-    
         document.querySelector('#btn_about').textContent = 'About Us';
         document.querySelector('#btn_areas').textContent = 'Practice Areas';
         document.querySelector('#btn_team').textContent = 'Our Team';
         document.querySelector('#btn_post').textContent = 'Posts';
         document.querySelector('#btn_contact').textContent = 'Contact Us';
+
+        /* Titulos de secciones */
+        translate('#about_box-about-title', 'About Us');
+        translate('#info_box-section-title', 'Practice Areas');
+        translate('#assoc_box-assoc-title', 'Associations');
+        translate('#team_box-team-title', 'Our Team');
+        translate('#post_box-post-title', 'Posts');
+        translate('#contact_box-contact-title', 'Contact Us');
+
+        /* Otros */
+
+        translate('#about_text', 
+            "'CYT Abogados' was founded by young professionals, highly trained and experienced in various areas of the legal profession, providing services to both individuals and companies, with a service vocation and results orientation. With continuous and permanent training, we seek to advise our clients by working closely and offering added value with technical quality in the study's practice areas."
+        );
+
         
     }
+}
+
+function translate(elementSelected, translation){
+    document.querySelector(elementSelected).textContent = translation;
 }
